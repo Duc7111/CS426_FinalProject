af9788a0a36bf23aece21e9e4436c876d5a51f9e
phungptn <ngothaiphung412@gmail.com>
2023-09-04 02:45:17 +0700

    update some colors

--- app/src/main/java/com/example/ash/MainActivity.kt
+++ app/src/main/java/com/example/ash/MainActivity.kt
@@ -3,18 +3,33 @@ package com.example.ash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ash.ui.theme.ASHTheme
+import com.example.ash.ui.theme.DeepBlue
+import com.example.ash.ui.theme.TextWhite
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("onCreate()")
         setContent {
             ASHTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,20 +42,78 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart()")
+    }
 }
 
 @Composable
+fun Homescreen(name: String) {
+    Box(modifier = Modifier
+        .background(DeepBlue)
+        .fillMaxSize()
+    ){
+        Greeting(name)
+    }
+
+}
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Good morning, $name",
+                    color = TextWhite,
+                    //style = MaterialTheme.typography.h2
+                )
+                Text(
+                    text = "We wish you have a good day!",
+                    color = TextWhite,
+                    //style = MaterialTheme.typography.body1
+                )
+            }
+        }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ASHTheme {
-        Greeting("Android")
+        Homescreen(name = "Andrew")
     }
 }
