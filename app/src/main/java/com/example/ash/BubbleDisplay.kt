d63a5177f8d791e5771fa6eab0252d59160f0329
Duc7111 <phantrungduc2003@gmail.com>
2023-09-18 09:11:35 +0700

    minnor changes

--- app/src/main/java/com/example/ash/BubbleDisplay.kt
+++ app/src/main/java/com/example/ash/BubbleDisplay.kt
@@ -1,39 +1,19 @@
 package com.example.ash
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ash.ui.theme.ASHTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 class BubbleDisplay: AppCompatActivity(){
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -56,7 +36,7 @@ class BubbleDisplay: AppCompatActivity(){
 
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
-fun bubbleDisplay() {
+private fun bubbleDisplay() {
     val bubbleViewModel = viewModel<BubbleViewModel>()
 
     LaunchedEffect(Unit) {
